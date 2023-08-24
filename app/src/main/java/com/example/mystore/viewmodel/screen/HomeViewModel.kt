d9e302d1d4d1d5ab173d4a92ddd14b36cc66b656
CIANDT\raphaellima <raphael.lima-ext@ab-inbev.com>
2023-08-23 23:56:31 -0300

    Get resume

--- app/src/main/java/com/example/mystore/viewmodel/screen/HomeViewModel.kt
+++ app/src/main/java/com/example/mystore/viewmodel/screen/HomeViewModel.kt
@@ -17,9 +17,33 @@ class HomeViewModel : CommonViewModel() {
         getListOfProducts()
     }
 
+//    val debits: Double = 100.0,
+//    val grossRevenue: Double = 200.0,
+//    val netRevenue: Double = 0.0,
+//    val stockValue: Double = 2000.0,
+
     // Todo - Aqui eu vou pegar as transações e produtos e manipular os dados para criar um objeto Resume
     private fun getResume() {
-        resume.value = Resume()
+        val debits = listOfPurchases.value.sumOf { transaction ->
+            transaction.transactionAmount
+        }
+
+        val grossRevenue = listOfSales.value.sumOf { transaction ->
+            transaction.transactionAmount
+        }
+
+        val netRevenue = grossRevenue - debits
+
+        val stockValue = listOfProducts.value.sumOf { product ->
+            product.salePrice * product.quantity
+        }
+
+        resume.value = Resume(
+            debits = debits,
+            grossRevenue = grossRevenue,
+            netRevenue = netRevenue,
+            stockValue = stockValue,
+        )
     }
 
     fun setImageRequestState(state: States) {
